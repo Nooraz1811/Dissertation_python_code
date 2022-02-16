c089eee0e5da7040e061d9ecc79f18c2fd684d42
ayaz <ayazrom14@gmail.com>
2022-02-16 20:02:24 +0400

    initial commit

---
+++ FaceRecognition.py
@@ -0,0 +1,71 @@
+import face_recognition
+import smtplib
+import os
+from picamera import PiCamera
+from time import sleep
+from gpiozero import MotionSensor,LED
+from signal import pause
+
+pir = MotionSensor(23)
+red = LED(22)
+red.on()
+camera= PiCamera()
+ 
+ 
+def FaceRecognition():
+    ayaz_image = face_recognition.load_image_file("/home/pi/Desktop/work/Ayaz.jpg")
+    unknown_image = face_recognition.load_image_file("/home/pi/Desktop/work/Face.jpg")
+
+    try:
+        ayaz_face_encoding = face_recognition.face_encodings(ayaz_image)[0]
+        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+    
+    except IndexError:
+        print("No face detected...Aborting process...")
+        return
+
+    known_faces = [ayaz_face_encoding]
+    results = face_recognition.compare_faces(known_faces,unknown_face_encoding)
+
+    if False in results:
+        os.system('echo "Am sorry, your face does not match, please contact MR AYAAZ for access" |festival --tts')
+        print("Face does not match!")
+        smtpUser = 'raspberrypi7786@gmail.com'
+        smtpPass = '8209257+A'
+
+        toAdd = 'ayazrom14@gmail.com'
+        fromAdd = smtpUser
+
+        subject = 'IMPORTANT MAIL FROM RASPBERRYPI4'
+        header = 'To:' + toAdd + '\n' + 'From:' + fromAdd + '\n' + 'Subject:' + subject
+        body = 'Unrecognised face detected at front door.'
+        print (header + '\n' + body)
+
+        s = smtplib.SMTP('smtp.gmail.com',587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(smtpUser,smtpPass)
+        print("Login Successfull")
+        s.sendmail(fromAdd,toAdd,header + '\n\n' + body)
+        s.quit()
+    else:
+        os.system('echo "Welcome home MR AYAAZ, your door is now open" |festival --tts')
+        print("Face Recognized is: Ayaaz!")
+        print("Door opened!")
+        red.off()
+        sleep(5)
+        os.system('echo "Attention! door is now closing" |festival --tts')
+        red.on()
+        print("Door closed!")
+def take_photo():
+    os.system('echo "Hello, please look at the camera while i take a photo of your face" |festival --tts')
+    camera.start_preview()
+    sleep(3)
+    camera.capture('/home/pi/Desktop/work/Face.jpg')
+    camera.stop_preview()
+    FaceRecognition()
+    sleep(5)
+#function that runs when motion is detected!
+pir.when_motion = take_photo
+pause() 
---
+++ PlateNumDetect.py
@@ -0,0 +1,80 @@
+from gpiozero import MotionSensor,LED
+from picamera import PiCamera
+from time import sleep
+from signal import pause
+import boto3
+import smtplib
+import os
+
+pir = MotionSensor(24)
+blue = LED(22)
+blue.on()
+camera = PiCamera()
+
+def PlateRecognition():
+    access_key_id = 'AKIARD2ZDU2CVNGZUT7T'
+    secret_access_key = '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'
+
+    photo = ('/home/pi/Desktop/work/plateNum1.jpg')
+    client = boto3.client('rekognition', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key,region_name='us-west-2')
+
+    with open (photo, 'rb') as source_image:
+        source_bytes = source_image.read()       
+        response = client.detect_text(Image ={'Bytes': source_bytes})
+
+    textDetections=response['TextDetections']
+    z=0
+    x=""
+    for text in textDetections:
+        y = (" " + text['DetectedText'])
+        x += y
+        z += 1
+        if z == 2:
+            break
+    l = x.split()
+    k = []
+    for i in l:
+        if (x.count(i)>1 and (i not in k)or x.count(i)==1):
+            k.append(i)
+    PlateNumber = (' '.join(k))
+    print("License Plate Detected is: " + PlateNumber)
+    if PlateNumber == "2935 CZ 15":
+        os.system('echo "Welcome home MR AYAAZ, your gate is now opening." |festival --tts')
+        print("Gate Opening...!")
+        blue.off()
+        sleep(5)
+        os.system('echo "Attention! gate is now closing." |festival --tts')
+        blue.on()
+        print("Gate Closed...!")
+    elif len(PlateNumber) < 1:
+        print("No plate number detected...!")
+    else:
+        os.system('echo "Am sorry, your plate number does not match, please contact MR AYAAZ for access." |festival --tts')
+        smtpUser = 'raspberrypi7786@gmail.com'
+        smtpPass = '8209257+A'
+        toAdd = 'ayazrom14@gmail.com'
+        fromAdd = smtpUser
+        subject = 'IMPORTANT MESSAGE FROM RASPBERRYPI4'
+        header = 'To:' + toAdd + '\n' + 'From:' + fromAdd + '\n' + 'Subject:' + subject
+        body = ('UNRECOGNISED VEHICLE DETECTED AT GATE: '+ PlateNumber)
+        print (header + '\n' + body)
+        s = smtplib.SMTP('smtp.gmail.com',587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(smtpUser,smtpPass)
+        print("Login Successfull")
+        s.sendmail(fromAdd,toAdd,header + '\n\n' + body)
+        s.quit()
+
+def take_photo():
+    os.system('echo "Hello, please wait while i take a photo of your license plate." |festival --tts')
+    camera.start_preview()
+    sleep(3)
+    camera.capture('/home/pi/Desktop/work/plateNum1.jpg')
+    camera.stop_preview()
+    PlateRecognition()
+    sleep(5)
+#function that runs when motion is detected!
+pir.when_motion = take_photo
+pause()
